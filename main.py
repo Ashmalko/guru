ca512a065c35c28a2037d5f1b03e44fcd59abcb2
Shmalko_a <a@ashmalko>
2025-12-02 22:07:44 +0400

    123

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-import pytest
-from selene import browser, be, have
-@pytest.fixture(
-def test_valid_login():
-    browser.open('https://software-testing.ru/lms/login/index.php')
-    browser.element('[id="username"]').type('leha777')
-    browser.element('[id="password"]').type('97916870').press_enter()
-    browser.element('[id="label_1_1"]').should(have.text('Личный кабинет'))
+from selene import browser, have
+
+def test_success_login():
+    browser.open('https://niffler.qa.guru')
+    browser.element('[id="username"]').type('stas')
+    browser.element('[id="password"]').type('12345')
+    browser.element('[id="login-button"]').click()
+
+    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
     browser.quit()
 
+def test_success_login_with_press_enter():
+    browser.open('https://niffler.qa.guru')
+    browser.element('[id="username"]').type('stas')
+    browser.element('[id="password"]').type('12345').press_enter()
 
-def test_wrong_login():
-    browser.open('https://software-testing.ru/lms/login/index.php')
-    browser.element('[id="username"]').type('leha777')
-    browser.element('[id="password"]').type('9791687.').press_enter()
-    browser.element('[class="error"]').should(have.text('Неверный логин или пароль, попробуйте заново.'))
+    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
     browser.quit()
-# browser.element('[id="search"]').should(have.text('QA.GURU: Курсы тестировщиков'))
+
+def test_wrong_credentials():
+    browser.open('https://niffler.qa.guru')
+    browser.element('[id="username"]').type('stas')
+    browser.element('[id="password"]').type('12345sgsrgwr').press_enter()
+
+    browser.element('[class="form__error"]').should(have.text('Bad credentials'))
+    browser.quit()
